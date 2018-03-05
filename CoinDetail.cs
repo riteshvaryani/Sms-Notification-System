d7ecdc5956d38236c30bba7d33f8562507f15f03
Ritesh Varyani <riva@microsoft.com>
2018-03-04 23:32:20 -0800

    initial commit for twilio support for notifications

---
+++ CoinDetail.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace TwlioSupport
+{
+    [DataContract]
+    public class CoinDetail
+    {
+        [DataMember]
+        public string id { get; private set; }
+
+        [DataMember]
+        public string name { get; private set; }
+
+        [DataMember]
+        public string symbol { get; private set; }
+
+        [DataMember]
+        public string rank { get; private set; }
+
+        [DataMember]
+        public string price_usd { get; private set; }
+
+        [DataMember (Name = "24h_volume_usd")]
+        public string h24_volume_usd { get; private set; }
+
+        [DataMember]
+        public string market_cap_usd { get; private set; }
+
+        [DataMember]
+        public string available_supply { get; private set; }
+
+        [DataMember]
+        public string total_supply { get; private set; }
+
+        [DataMember]
+        public string max_supply { get; private set; }
+
+        [DataMember]
+        public string percent_change_1h { get; private set; }
+
+        [DataMember]
+        public string percent_change_24h { get; private set; }
+
+        [DataMember]
+        public string percent_change_7d { get; private set; }
+
+        [DataMember]
+        public string last_updated { get; private set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// Download the twilio-csharp library from twilio.com/docs/libraries/csharp
+using System;
+using System.Configuration;
+using System.Net.Http;
+
+using Newtonsoft.Json;
+
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+using TwlioSupport;
+
+class Example
+{
+    static void Main(string[] args)
+    {
+        GetQuotes();
+        //SendMessage();
+        
+    }
+
+    private static void SendMessage()
+    {
+        // Find your Account Sid and Auth Token at twilio.com/console
+        string accountSid = ConfigurationManager.AppSettings["AccountSid"];
+        string authToken = ConfigurationManager.AppSettings["AuthToken"];
+        TwilioClient.Init(accountSid, authToken);
+
+        var to = new PhoneNumber(ConfigurationManager.AppSettings["ToPhoneNumber"]);
+        var message = MessageResource.Create(
+            to,
+            from: new PhoneNumber(ConfigurationManager.AppSettings["FromPhoneNumber"]),
+            body: "This is the ship that made the Kessel Run in fourteen parsecs?");
+
+        Console.WriteLine(message.Sid);
+    }
+
+    private static void GetQuotes()
+    {
+        using (var client = new HttpClient())
+        {
+            var result = client.GetAsync("https://api.coinmarketcap.com/v1/ticker/");
+            var resultAsJson = result.Result.Content.ReadAsStringAsync().Result;
+            var deserializedObject = JsonConvert.DeserializeObject<CoinDetail[]>(resultAsJson);
+            Console.WriteLine(JsonConvert.SerializeObject(deserializedObject[0]));
+        }
+        //var xhr = new XMLHttpRequest();
+        //xhr.open("GET", "https://api.coinmarketcap.com/v1/ticker/", false);
+        //xhr.send();
+    }
+}
